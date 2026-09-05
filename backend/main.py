0628e73ac7b4b48ba2e9f78aeb2250bc9da08733
chidimma-okoli <chidimmaokoli3294@gmail.com>
2026-09-05 21:10:58 +0100

    Update main.py file

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://16.192.149.100"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
